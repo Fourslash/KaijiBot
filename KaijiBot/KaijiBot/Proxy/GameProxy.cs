60fa2c957c11cdba5ae1be2597aef082f7f176a5
Fourslash <fourslash@gmail.com>
2016-04-30 02:34:27 +0400

    basic proxy

---
+++ KaijiBot/KaijiBot/Proxy/GameProxy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiddler;
+using System.IO;
+
+namespace KaijiBot.Proxy
+{
+    class GameProxy
+    {
+        private int ProcessID {get; set;}
+        public delegate void UI(string jsonString, string apiString);
+        public static event UI NewDataCollected;
+
+        public GameProxy(int processId)
+        {
+            this.ProcessID = processId;
+            FiddlerStart();
+
+        }
+        public void Dispose()
+        {
+            //LogWriter.WriteLog("Shutting down Fiddler");
+            Fiddler.FiddlerApplication.Shutdown();
+        }
+        void FiddlerStart()
+        {
+            try
+            {
+
+                #region settings
+
+                FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);
+                FiddlerCoreStartupFlags oFCSF = FiddlerCoreStartupFlags.Default;
+                //FiddlerCoreStartupFlags.
+                Fiddler.FiddlerApplication.Startup(0, oFCSF);
+                #endregion
+
+                Fiddler.FiddlerApplication.BeforeRequest += BeforeRequest;
+                Fiddler.FiddlerApplication.BeforeResponse += BeforeResponse;
+                //LogWriter.WriteLogSucces("Fiddler started");
+            }
+            catch (Exception ex)
+            {
+                //LogWriter.WriteLogOnException(ex);
+                throw ex;
+            }
+
+        }
+
+        void BeforeRequest(Session oSession)
+        {
+            /*if (oSession.LocalProcess.ToLower().Contains("skypebot2"))
+            {
+                oSession.Ignore();
+            }*/
+        }
+
+        void BeforeResponse(Session oS)
+        {
+            if (IsSessionNeedsProcessed(oS))
+            {
+                ProcessSession(oS);
+            }
+        }
+
+        bool IsSessionNeedsProcessed(Session oS)
+        {
+            if (oS.LocalProcessID != this.ProcessID)
+            {
+                return false;
+            } else if (!oS.oResponse.MIMEType.Equals("application/json"))
+            {
+                return false;
+            } else if (!oS.PathAndQuery.Contains("/casino_poker"))
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+
+        }
+
+        void ProcessSession(Session oS)
+        {
+            oS.utilDecodeResponse();
+
+            using (Stream receiveStream = new MemoryStream(oS.ResponseBody))
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    //Console.WriteLine(string.Format("Processing {0}", oS.PathAndQuery));
+                    string tmp = readStream.ReadToEnd();
+                    Logger.LoggerContoller.ProxyLogger.Debug(tmp);
+                    //string jsonStr = readStream.ReadToEnd().Remove(0, 7);
+                    //string apiStr = oS.PathAndQuery;
+                    //var json = DynamicJson.Parse(str);
+                    //NewDataCollected(jsonStr, apiStr);
+
+                }
+            }
+        }
+    }
+}
---
+++ KaijiBot/KaijiBot/Proxy/ProcessConnector.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace KaijiBot.Proxy
+{
+    class ProcessNotFoundException : Exception
+    {
+        public ProcessNotFoundException(string name) : base(ModifyMessage(name)) {  }
+
+        private static string ModifyMessage(string name)
+        {
+            return string.Format("Cant find process by name {0}", name);
+        }
+    }
+    class ProcessClosedException : Exception
+    {
+        public ProcessClosedException(string name) : base(ModifyMessage(name)) { }
+
+        private static string ModifyMessage(string name)
+        {
+            return string.Format("Process {0} terminated", name);
+        }
+    }
+
+    class ToManyProcessesException : Exception
+    {
+        Process[] processes;
+        public ToManyProcessesException(string name, Process[] processes) 
+            : base(ModifyMessage(name)) {
+            this.processes = processes;
+        }
+
+        private static string ModifyMessage(string name)
+        {
+           return string.Format("Found too many processes with name {0}", name);
+        }
+    }
+
+    class ProcessConnector
+    {
+
+        private Process process { get; set; }
+        private string name { get; set; }
+
+        public ProcessConnector()
+        {
+
+        }
+        public int Connect(string name)
+        {
+            Logger.LoggerContoller.ProcessLogger
+                .Debug(string.Format("Findnding process \"{0}\"", name));
+            this.process = FindProcess(name);
+            this.name = name;
+            process.EnableRaisingEvents = true;
+            process.Exited += Process_Exited;
+
+            return process.Id;
+        }
+
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            throw new ProcessClosedException(name);
+        }
+
+        private Process FindProcess(string name)
+        {
+            var processes = Process.GetProcessesByName(name);
+            Logger.LoggerContoller.ProcessLogger
+                .Verbose(string.Format("Found: {0} by name: {1}", processes.Length, name));
+            processes = processes.Where(p => p.MainWindowTitle.IndexOf("ChromeApps") != -1).ToArray();
+            Logger.LoggerContoller.ProcessLogger
+                .Verbose(string.Format("Left after filter: {0}", processes.Length));
+            if (processes.Length == 0)
+            {
+                throw new ProcessNotFoundException(name);
+            } else if (processes.Length > 1)
+            {
+                throw new ToManyProcessesException(name, processes);
+            } else
+            {
+                return processes[0];
+            }
+        }
+    }
+}
