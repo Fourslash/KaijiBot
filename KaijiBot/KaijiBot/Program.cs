e51e4f12072d551c9df579f35c9581e29b15559e
Fourslash <fourslash@gmail.com>
2016-05-15 19:18:52 +0400

    proper exception handling

--- KaijiBot/KaijiBot/Program.cs
+++ KaijiBot/KaijiBot/Program.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Threading;
 using KaijiBot.Settings;
-
-
+using System.Security.Permissions;
 using System.Diagnostics;
 
 
@@ -15,31 +14,34 @@ namespace KaijiBot
 {
     class Program
     {
+
+        [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.ControlAppDomain)]
         static void Main(string[] args)
         {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandedExHandler);
             Config.Load();
             new Thread(() =>
             {
                 Thread.CurrentThread.IsBackground = true;
                 new LowLevelBullshit.KeyHook();
             }).Start();
-            try {
-                var connector = new Proxy.ProcessConnector();
-                var prc = connector.Connect("chrome");
-                var ts = new Proxy.GameProxy(prc);
-                var gameEventEmmiter = new Game.EventEmitter(ts);
-                var table = new Game.Table(gameEventEmmiter);
-                Logger.LoggerContoller.MainLogger.Info(
-                    "Bot started. Press \"Q\" to exit");
-                while (true) ;
-            } catch (Exception ex)
-            {
-                Logger.LoggerContoller.MainLogger.Error(ex);
-                Logger.LoggerContoller.MainLogger.Info("Press any key to exit");
-                Console.ReadKey();
-            }
-           
+            var connector = new Proxy.ProcessConnector();
+            var prc = connector.Connect("chrome");
+            var ts = new Proxy.GameProxy(prc);
+            var gameEventEmmiter = new Game.EventEmitter(ts);
+            var table = new Game.Table(gameEventEmmiter);
+            Logger.LoggerContoller.MainLogger.Info(
+                "Bot started. Press \"Q\" to exit");
+            while (true) ;
+        }
 
-        }       
+        static void UnhandedExHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception ex = (Exception)args.ExceptionObject;
+            Logger.LoggerContoller.MainLogger.Error(ex);
+            Logger.LoggerContoller.MainLogger.Info("Press any key to exit");
+            Console.ReadKey();
+        }
     }
 }
